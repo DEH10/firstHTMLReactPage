8ea64f46ecb46b6b854aa05d9eb770638dd34ce9
DEH <156702447+DEH10@users.noreply.github.com>
2024-05-19 03:11:24 +0000

    your_commit_message

--- react-apps/src/App.js
+++ react-apps/src/App.js
@@ -1,9 +1,22 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
 
 function App(props) {
   const [counter, setCounter] = useState(0);
+  const [breeds, setBreeds] = useState([]);
+
+  useEffect(() => {
+    const url = "https://dogapi.dog/api/v2/breeds";
+    axios.get(url)
+      .then(response => {
+        setBreeds(response.data.data);
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error.message);
+      });
+  }, []);
 
   const incrementCounter = () => {
     setCounter(counter + 1);
@@ -25,6 +38,22 @@ function App(props) {
         <br />
         <br />
         {counter}
+      </div>
+
+      {/* Rendering the list of breeds */}
+      <div>
+        <h2>List of Dog Breeds</h2>
+        <ul>
+          {breeds.map((breed, index) => (
+            <li key={index}>
+              <h3>{breed.attributes.name}</h3>
+              <p>Life Span: {breed.attributes.life.min} - {breed.attributes.life.max} years</p>
+              <p>Male Weight: {breed.attributes.male_weight.min} - {breed.attributes.male_weight.max} kg</p>
+              <p>Female Weight: {breed.attributes.female_weight.min} - {breed.attributes.female_weight.max} kg</p>
+              <p>Hypoallergenic: {breed.attributes.hypoallergenic ? 'Yes' : 'No'}</p>
+            </li>
+          ))}
+        </ul>
       </div>
     </div>
   );
